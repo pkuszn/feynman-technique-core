188644e45e65e0120ecbfd52a5296ef02b65933b
pkuszneruk <pkuszneruk@10305-ARD>
2023-06-01 17:16:01 +0200

    added dtos and models

---
+++ src/feynman-technique-core/config.py
@@ -0,0 +1,50 @@
+import os
+import yaml
+import pathlib
+from copy import deepcopy
+
+def load_config(mode=os.environ.get('FLASK_ENV')):
+    path = pathlib.Path(__file__).parent
+    try:
+        match mode:
+            case "PRODUCTION":
+                load = None
+                with open(f"{path}/config/prod.yaml") as stream:
+                    try: 
+                        load = yaml.safe_load(stream)
+                    except yaml.YAMLError as exc:
+                        print(exc) 
+                return load
+            case "DEVELOPMENT":
+                load = None
+                with open(f"{path}/config/dev.yaml") as stream:
+                    try: 
+                        load = yaml.safe_load(stream)
+                    except yaml.YAMLError as exc:
+                        print(exc)
+                return load
+    except ImportError:
+        load = None
+        with open(f"{path}/config/dev.yaml") as stream:
+            try: 
+                load = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+        return load
+
+def mask(load, parents, names):
+    for parent in parents:
+
+        for k, v in load[parent].items():
+            
+            if len(names) > 1:
+                for name in names:
+                    if k == name:
+                        load[parent][k] = '************'
+                        
+            else:
+                if k == names:
+                    load[parent][k] = '************'
+
+    return deepcopy(load)
+    
---
+++ src/feynman-technique-core/dtos.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass
+
+class Token:
+    __slots__ = ["internal_id", "text", "lemma", "upos", "head", "deprel", "feats", "status", "complex", "root", "child"]
+    
+    def __init__(self, internal_id, text, lemma, upos, head, deprel, feats=None, status=False, complex=False):
+        self.internal_id = internal_id # phrase identifier in request
+        self.text = text
+        self.lemma = lemma
+        self.upos = upos #part of speech
+        self.head = head #pointer to the root element according to dependency parsing mech, if head == 0 then element is root
+        self.deprel = deprel #dependency parsing tag
+        self.feats = feats
+        self.status = status # informs about presence in database
+        self.root = None #head node used to build complex questions
+        self.child = [] #child of head used to build complex questions
+        self.complex = complex
+        
+    def __str__(self):
+        return f"{self.internal_id}, {self.text}, {self.lemma}, {self.upos}, {self.head}, {self.deprel}, {self.status}, {self.feats}, {self.complex}, {self.root}"   
+
+@dataclass
+class QuestionDto:
+    id: int
+    level: int
+    sentence: str
+    parent_id: int
+    question: str
+    word_length: int
+    children: list
+    
+    def __str__(self):
+        return f"{self.id}, {self.level}, {self.sentence}, {self.parent_id}, {self.question}, {self.word_length}"
+
+@dataclass
+class ResponseDto:
+    questions: list
+    understood_sentences: list
+
+@dataclass
+class WordPresentationDto:
+    id: int
+    word: str
+    part_of_speech: int
+    part_of_speech_name: str
+    created_date: str
+    context: str
+    link: str
