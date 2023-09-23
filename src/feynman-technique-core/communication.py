8e76d10c4558b354915ea0e044a0d7c068c8fe43
patryk <patryk.kuszneruk1798@gmail.com>
2023-09-23 21:08:57 +0200

    get words from rest api works

--- src/feynman-technique-core/communication.py
+++ src/feynman-technique-core/communication.py
@@ -7,13 +7,13 @@ config.read('./config/config.ini')
 
 FEYNMAN_TECHNIQUE_BACKEND_REST_API_BASE_URL = config['restapi']['feynman-technique-backend']
 FEYNMAN_TECHNIQUE_SCRAPER_REST_API_BASE_URL = config['restapi']['feynman-technique-scraper']
+headers= {'Content-Type': 'application/json'}
 
 async def get_words_async() -> list[str]:
     url = f'{FEYNMAN_TECHNIQUE_BACKEND_REST_API_BASE_URL}{BackendRestApi.GetWordsOnly}'
     #TODO: enable SSL verification
     # ssl_context = ssl.create_default_context(cafile=certifi.where())
-    async with ClientSession(connector=TCPConnector(ssl=False)) as session:
-        async with session.get(url, allow_redirects=True) as response:
-            response = await response.read()
-            print(response)
+    async with ClientSession(connector=TCPConnector(ssl=False), headers=headers) as session:
+        async with session.post(url, allow_redirects=True) as response:
+            response = await response.json()
     return response
